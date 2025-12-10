ce0ee9edd10db294d780686768ce68977eda06e7
AmarDilig <amar.kajevic@dilig.net>
2025-12-10 10:24:43 +0100

    fixed some things 3

--- src/components/patients/PatientPage.tsx
+++ src/components/patients/PatientPage.tsx
@@ -5,16 +5,21 @@ import AddPatientForm from "./AddPatientForm";
 
 export default function PatientsPage() {
   return (
-    <div className="flex">
-      <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Pacijenti</h1>
-      <AddPatientForm />
-      
-    </div>
-    <div>
-      <PatientList/>
-    </div>
-    
+     <div className="w-full px-4">
+
+      {/* NASLOV */}
+      <h1 className="text-2xl font-bold mb-4 text-center">Pacijenti</h1>
+
+      {/* FORMA — CENTRIRANA I UŽA */}
+      <div className="max-w-4xl mx-auto mb-8">
+        <AddPatientForm />
+      </div>
+
+      {/* LISTA PACIJENATA — PUNA ŠIRINA */}
+      <div className="w-full">
+        <PatientList />
+      </div>
+
     </div>
     
     
