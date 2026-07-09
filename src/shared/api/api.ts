a59df3deeb4501eb364f23a407c4fd5a6bef4a3b
Amar <kajevicamar997@gmail.com>
2026-07-10 01:10:05 +0200

    replaced all localhost with verce fixed error

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://medikalija-frontend.vercel.app",
+  baseURL: "https://medikalija-api.vercel.app",
 });
 
 api.interceptors.request.use((config) => {
